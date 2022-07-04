b66b738a91f1365531863435589b598c72fa85c2
JackiieM <jackie.nct@gmail.com>
2022-07-04 15:02:53 -0400

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import validator from './validator.js';
 
-const validateBtn = document.getElementById("btn")
-btn.addEventListener("click", validateCard)
+const validateBtn = document.getElementById("btn");
+//const mask = validator.maskify(creditCardNumber);
 
 function validateCard() {
-
-let creditCardNumber = document.getElementById("cardNumb");
+    const creditCardNumber = document.getElementById("cardNumb").value;
     if(validator.isValid(creditCardNumber) === true) {
-        alert ('valida')
-    } else {
-    alert("invalida")
+        alert ('Tu tarjeta es valida.')
+        
+     } else {
+       alert("invalida")
     }
 
 }
+
+
+validateBtn.addEventListener("click", validateCard)
